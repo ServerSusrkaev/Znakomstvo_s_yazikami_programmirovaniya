4cab8253a969eba053e296944211a75ef086cbb6
ServEr <Susurkaev@yandex.ru>
2023-03-13 13:27:34 +0300

    Отредактирован код задачи: Строка с наименьшей суммой элементов двумерного массива

--- 3. Домашние задания/Строка с наименьшей суммой элементов двумерного массива/Program.cs
+++ 3. Домашние задания/Строка с наименьшей суммой элементов двумерного массива/Program.cs
@@ -1,17 +1,17 @@
 ﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку
 // с наименьшей суммой элементов.
 // Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
+// 8 4 4 8 
 // 5 2 6 7
+// 1 1 1 1
+// 5 9 2 3
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-int[,] arrayInitial = new int[4,4] {{1, 4, 7, 2}, {5, 9, 2, 3}, {1, 4, 2, 4}, {5, 2, 6, 7}};
+int[,] arrayInitial = new int[4,4] {{5, 2, 6, 7}, {8, 4, 4, 8}, {1, 1, 1, 1}, {5, 9, 2, 3}};
 int[] arrayFinal = new int[arrayInitial.GetLength(1)];
 
+//Заполненеи массива arrayFinal сумой числе каждой строки массива arrayInitial
 int sum = 0;
-
 for (int i = 0; i < arrayInitial.GetLength(0); i++)
 {
     for (int j = 0; j < arrayInitial.GetLength(1); j++)
@@ -19,20 +19,19 @@ for (int i = 0; i < arrayInitial.GetLength(0); i++)
         sum  += arrayInitial[i,j];
     }
     arrayFinal[i] = sum;
-    //System.Console.WriteLine(sum);
     sum = 0;
 }
 
-int minSumIndex = arrayFinal[0];
+int minSumIndex = 1;
+int minSum = arrayFinal[0];
 
-for (int i = 0; i < arrayFinal.Length; i++)
+for (int i = 1; i < arrayFinal.Length; i++)
 {
-    System.Console.Write(arrayFinal[i]);
-    System.Console.Write(" ");
-    if (minSumIndex > arrayFinal[i])
+    if (minSum > arrayFinal[i])
     {
-        minSumIndex = i+1;
+        minSumIndex = i + 1;
+        minSum = arrayFinal[i];
     }
 }
-System.Console.WriteLine(" ");
+System.Console.WriteLine("");
 System.Console.Write("Строка с наименьшей суммой элементов №: " + minSumIndex);
